2772a8c7c42bc886b956b35972def0fcdc9a2613
Qspc <shibgotalloh_qqq@apps.ipb.ac.id>
2021-11-12 22:08:34 +0700

    push template

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
 });
