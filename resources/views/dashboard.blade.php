cbfe3db1b421734079eb448c4be8ffc9336feed4
Qspc <shibgotalloh_qqq@apps.ipb.ac.id>
2021-11-18 00:20:50 +0700

    dashboard mulai

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -39,17 +39,16 @@
                             <button class="btn btn-link text-white pl-0" type="button" data-toggle="sidebar">
     <i class="material-icons align-middle md-36">short_text</i>
   </button>
-                            <div class="page-title m-0">Financial</div>
 
                             <div class="collapse navbar-collapse" id="mainNavbar">
                                 <ul class="navbar-nav ml-auto align-items-center">
                                     <li class="nav-item nav-link">
                                         <div class="form-group m-0">
                                             <div class="input-group input-group--inline">
-                                                <div class="input-group-addon">
+                                                {{-- <div class="input-group-addon">
                                                     <i class="material-icons">search</i>
                                                 </div>
-                                                <input type="text" class="form-control" name="search" placeholder="Search app..">
+                                                <input type="text" class="form-control" name="search" placeholder="Search app.."> --}}
                                             </div>
                                         </div>
                                     </li>
@@ -261,40 +260,7 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="card card-earnings">
-                            <div class="card-group">
-                                <div class="card card-body mb-0">
-                                    <div class="media align-items-center">
-                                        <div class="media-body">
-                                            <p class="card-text text-muted mb-1">Earned today</p>
-                                            <h1 class="mb-0 font-weight-normal">&dollar;6,120</h1>
-                                        </div>
-                                        <div class="badge badge-success">+52%</div>
-                                    </div>
-                                </div>
-                                <div class="card card-body mb-0">
-                                    <div class="media align-items-center">
-                                        <div class="media-body">
-                                            <p class="card-text text-muted mb-1">Earned this week</p>
-                                            <h1 class="mb-0 font-weight-normal">&dollar;14,276</h1>
-                                        </div>
-                                        <div class="badge badge-primary">+16%</div>
-                                    </div>
-                                </div>
-                                <div class="card card-body mb-0">
-                                    <div class="media align-items-center">
-                                        <div class="media-body">
-                                            <p class="card-text text-muted mb-1">Earned this month</p>
-                                            <h1 class="mb-0 font-weight-normal">&dollar;39,882</h1>
-                                        </div>
-                                        <div class="badge badge-warning">+5%</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="p-2 bg-primary">
-                                <div id="morris-area-chart" style="width: 100%; height: 250px;"></div>
-                            </div>
-                        </div>
+
 
                         <div class="row">
                             <div class="col-lg-4">
@@ -787,23 +753,23 @@
                         <ul class="drawer-menu" id="dasboardMenu" data-children=".drawer-submenu">
                             <li class="drawer-menu-item active ">
                                 <a href="index.html">
-        <i class="material-icons">poll</i>
-        <span class="drawer-menu-text"> Financial</span>
-      </a>
+                                    <i class="material-icons">poll</i>
+                                    <span class="drawer-menu-text"> Financial</span>
+                                </a>
                             </li>
                             <li class="drawer-menu-item">
                                 <a href="projects.html">
-        <i class="material-icons">dns</i>
-        <span class="drawer-menu-text"> Projects/Tickets</span>
-        <span class="badge badge-pill badge-success ml-1">4</span>
-      </a>
+                                    <i class="material-icons">dns</i>
+                                    <span class="drawer-menu-text"> Projects/Tickets</span>
+                                    <span class="badge badge-pill badge-success ml-1">4</span>
+                                </a>
                             </li>
                             <li class="drawer-menu-item ">
                                 <a href="retail.html">
-        <i class="material-icons">store</i>
-        <span class="drawer-menu-text"> E-Commerce</span>
-      </a>
-                            </li>
+                                    <i class="material-icons">store</i>
+                                    <span class="drawer-menu-text"> E-Commerce</span>
+                                </a>
+                            {{-- </li>
                             <li class="drawer-menu-item ">
                                 <a href="real-estate-grid.html">
         <i class="material-icons">business</i>
@@ -822,217 +788,7 @@
         <span class="drawer-menu-text"> Analytics</span>
       </a>
                             </li>
-                        </ul>
-
-                        <!-- HEADING -->
-                        <div class="py-2 drawer-heading">
-                            Components
-                        </div>
-
-                        <!-- COMPONENTS MENU -->
-                        <ul class="drawer-menu" id="componentsMenu" data-children=".drawer-submenu">
-                            <li class="drawer-menu-item drawer-submenu">
-                                <a data-toggle="collapse" data-parent="#componentsMenu" href="#" data-target="#uiComponentsMenu" aria-controls="uiComponentsMenu" aria-expanded="false" class="collapsed">
-        <i class="material-icons">library_books</i>
-        <span class="drawer-menu-text"> UI Components</span>
-      </a>
-                                <ul class="collapse " id="uiComponentsMenu">
-                                    <li class="drawer-menu-item "><a href="ui-buttons.html">Buttons</a></li>
-                                    <li class="drawer-menu-item "><a href="ui-colors.html">Colors</a></li>
-                                    <li class="drawer-menu-item "><a href="ui-grid.html">Grid</a></li>
-                                    <li class="drawer-menu-item "><a href="ui-icons.html">Icons</a></li>
-                                    <li class="drawer-menu-item "><a href="ui-typography.html">Typography</a></li>
-                                    <li class="drawer-menu-item "><a href="ui-drag-drop.html">Drag &amp; Drop</a></li>
-                                    <li class="drawer-menu-item "><a href="ui-loaders.html">Loaders</a></li>
-                                </ul>
-                            </li>
-
-
-                            <li class="drawer-menu-item drawer-submenu">
-                                <a data-toggle="collapse" data-parent="#componentsMenu" href="#" data-target="#formsMenu" aria-controls="formsMenu" aria-expanded="false" class="collapsed">
-        <i class="material-icons">text_format</i>
-        <span class="drawer-menu-text"> Forms</span>
-      </a>
-                                <ul class="collapse " id="formsMenu">
-                                    <li class="drawer-menu-item "><a href="form-controls.html">Form Controls</a></li>
-                                    <li class="drawer-menu-item "><a href="checkboxes-radios.html">Checkboxes &amp; Radios</a></li>
-                                    <li class="drawer-menu-item "><a href="switches-toggles.html">Switches &amp; Toggles</a></li>
-                                    <li class="drawer-menu-item "><a href="form-layout.html">Layout Variations</a></li>
-                                    <li class="drawer-menu-item "><a href="validation.html">Validation</a></li>
-                                    <li class="drawer-menu-item "><a href="custom-forms.html">Custom Forms</a></li>
-                                    <li class="drawer-menu-item "><a href="text-editor.html">Text Editor</a></li>
-                                    <li class="drawer-menu-item "><a href="datepicker.html">Datepicker</a></li>
-                                </ul>
-                            </li>
-                            <li class="drawer-menu-item  ">
-                                <a href="ui-tables.html">
-        <i class="material-icons">tab</i>
-        <span class="drawer-menu-text"> Tables</span>
-      </a>
-                            </li>
-                            <li class="drawer-menu-item  ">
-                                <a href="ui-notifications.html">
-        <i class="material-icons">notifications</i>
-        <span class="drawer-menu-text"> Notifications</span>
-      </a>
-                            </li>
-                            <li class="drawer-menu-item  ">
-                                <a href="charts.html">
-        <i class="material-icons">equalizer</i>
-        <span class="drawer-menu-text"> Charts</span>
-      </a>
-                            </li>
-                            <li class="drawer-menu-item  ">
-                                <a href="events-calendar.html">
-        <i class="material-icons">event_available</i>
-        <span class="drawer-menu-text"> Calendar</span>
-      </a>
-                            </li>
-                            <li class="drawer-menu-item  ">
-                                <a href="maps.html">
-        <i class="material-icons">pin_drop</i>
-        <span class="drawer-menu-text"> Maps</span>
-      </a>
-                            </li>
-                        </ul>
-
-
-                        <!-- HEADING -->
-                        <div class="py-2 drawer-heading">
-                            Pages
-                        </div>
-
-                        <!-- PAGES MENU -->
-                        <ul class="drawer-menu" id="pagesMenu" data-children=".drawer-submenu">
-                            <li class="drawer-menu-item">
-                                <a href="user-list.html">
-        <i class="material-icons">list</i>
-        <span class="drawer-menu-text">User List</span>
-      </a>
-                            </li>
-                            <li class="drawer-menu-item">
-                                <a href="search.html">
-        <i class="material-icons">search</i>
-        <span class="drawer-menu-text">Search</span>
-      </a>
-                            </li>
-                            <li class="drawer-menu-item">
-                                <a href="gallery.html">
-        <i class="material-icons">grid_on</i>
-        <span class="drawer-menu-text">Gallery</span>
-      </a>
-                            </li>
-                            <li class="drawer-menu-item">
-                                <a href="account.html">
-        <i class="material-icons">edit</i>
-        <span class="drawer-menu-text">Edit Account</span>
-      </a>
-                            </li>
-                            <li class="drawer-menu-item">
-                                <a href="login.html">
-        <i class="material-icons">lock</i>
-        <span class="drawer-menu-text">Login</span>
-      </a>
-                            </li>
-                            <li class="drawer-menu-item">
-                                <a href="signup.html">
-        <i class="material-icons">account_circle</i>
-        <span class="drawer-menu-text">Sign Up</span>
-      </a>
-                            </li>
-                            <li class="drawer-menu-item">
-                                <a href="forgot-password.html">
-        <i class="material-icons">help</i>
-        <span class="drawer-menu-text">Forgot Password</span>
-      </a>
-                            </li>
-
-                            <li class="drawer-menu-item drawer-submenu">
-                                <a data-toggle="collapse" data-parent="#pagesMenu" href="#" data-target="#errorsMenu" aria-controls="errorsMenu" aria-expanded="false" class="collapsed">
-        <i class="material-icons">error</i>
-        <span class="drawer-menu-text"> Error</span>
-      </a>
-                                <ul class="collapse " id="errorsMenu">
-                                    <li class="drawer-menu-item "><a href="error-403.html">Error 403</a></li>
-                                    <li class="drawer-menu-item "><a href="error-404.html">Error 404</a></li>
-                                    <li class="drawer-menu-item "><a href="error-500.html">Error 500</a></li>
-                                    <li class="drawer-menu-item "><a href="error-503.html">Error 503</a></li>
-                                </ul>
-                            </li>
-
-                            <li class="drawer-menu-item drawer-submenu">
-                                <a data-toggle="collapse" data-parent="#pagesMenu" href="#" data-target="#extrasMenu" aria-controls="extrasMenu" aria-expanded="false" class="collapsed">
-        <i class="material-icons">more</i>
-        <span class="drawer-menu-text"> Extra</span>
-      </a>
-                                <ul class="collapse " id="extrasMenu">
-                                    <li class="drawer-menu-item "><a href="shopping-cart.html">Shopping cart</a></li>
-                                    <li class="drawer-menu-item "><a href="product.html">Product</a></li>
-                                    <li class="drawer-menu-item "><a href="property.html">Property</a></li>
-                                    <li class="drawer-menu-item "><a href="project.html">Project</a></li>
-                                    <li class="drawer-menu-item "><a href="project-tasks.html">Project tasks</a></li>
-                                    <li class="drawer-menu-item "><a href="coming-soon.html">Coming soon</a></li>
-                                    <li class="drawer-menu-item "><a href="maintenance.html">Maintenance</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-
-                    </nav>
-                </div>
-            </div>
-        </div>
-        <!-- // END drawer -->
-
-        <!-- drawer -->
-        <div class="mdk-drawer js-mdk-drawer" id="user-drawer" data-position="right" data-align="end">
-            <div class="mdk-drawer__content">
-                <div class="mdk-drawer__inner" data-simplebar data-simplebar-force-enabled="true">
-                    <nav class="drawer drawer--light">
-                        <div class="drawer-spacer drawer-spacer-border">
-                            <div class="media align-items-center">
-                                <img src="https://pbs.twimg.com/profile_images/928893978266697728/3enwe0fO_400x400.jpg" class="img-fluid rounded-circle mr-2" width="35" alt="">
-                                <div class="media-body">
-                                    <a href="#" class="h5 m-0">Frontted</a>
-                                    <div>Account Manager</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="drawer-spacer bg-body-bg">
-                            <div class="d-flex justify-content-between mb-2">
-                                <p class="h6 text-gray m-0"><i class="material-icons align-middle md-18 text-primary">monetization_on</i> Balance</p>
-                                <span>$21,011</span>
-                            </div>
-                            <div class="d-flex justify-content-between">
-                                <p class="h6 text-gray m-0"><i class="material-icons align-middle md-18 text-primary">shopping_cart</i> Sales</p>
-                                <span>142</span>
-                            </div>
-                        </div>
-                        <!-- MENU -->
-                        <ul class="drawer-menu" id="userMenu" data-children=".drawer-submenu">
-                            <li class="drawer-menu-item">
-                                <a href="account.html">
-        <i class="material-icons">lock</i>
-        <span class="drawer-menu-text"> Account</span>
-      </a>
-                            </li>
-                            <li class="drawer-menu-item">
-                                <a href="profile.html">
-        <i class="material-icons">account_circle</i>
-        <span class="drawer-menu-text"> Profile</span>
-      </a>
-                            </li>
-                            <li class="drawer-menu-item">
-                                <a href="login.html">
-        <i class="material-icons">exit_to_app</i>
-        <span class="drawer-menu-text"> Logout</span>
-      </a>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </div>
-        </div>
-        <!-- // END drawer -->
+                        </ul> --}}
 
     </div>
     <!-- // END drawer-layout -->
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -267,16 +267,60 @@
                 <div class="row">
                     <div class="col-lg-12 text-center">
                         <h2 class="sec_title">F.A.Q</h2>
-                        <p class="sec_desc">
+                        {{-- <p class="sec_desc">
                             Simply import the sample files we ship with the theme and the core structure for your site is already<br>
                             mind that even if you don’t use the demo content, you’ll be much better off than with most other
-                        </p>
+                        </p> --}}
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col-lg-12 text-center">
 
+                        <div class="accordion" id="accordionExample">
+                            <div class="accordion-item">
+                              <h2 class="accordion-header" id="headingOne">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                  Accordion Item #1
+                                </button>
+                              </h2>
+                              <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                  <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                </div>
+                              </div>
+                            </div>
+                            <div class="accordion-item">
+                              <h2 class="accordion-header" id="headingTwo">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                  Accordion Item #2
+                                </button>
+                              </h2>
+                              <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                  <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                </div>
+                              </div>
+                            </div>
+                            <div class="accordion-item">
+                              <h2 class="accordion-header" id="headingThree">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                  Accordion Item #3
+                                </button>
+                              </h2>
+                              <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+
+                    </div>
+                </div>
             </div>
         </section>
         <!-- Common section -->
+
         <!-- footer section -->
         <footer class="footer footer_2">
             <div class="container">
@@ -334,6 +378,7 @@
             </div>
         </footer>
         <!-- footer section -->
+
         <!-- Copyright -->
         <section class="copyright copyright_2">
             <div class="container">
